a84dcc6893a5d5f6fba3fe97ec05ee02ec8d01b4
RUSHWAYC <rushwayc@yahoo.com>
2023-03-12 23:38:46 +0100

    Added CSS and color changing to the ball.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as Three from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
 import "./style.css";
 
 //Scene
@@ -9,6 +10,7 @@ const scene = new Three.Scene();
 const geometry = new Three.SphereGeometry(3, 64, 64);
 const material = new Three.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
 });
 const mesh = new Three.Mesh(geometry, material);
 scene.add(mesh);
@@ -22,6 +24,7 @@ const sizes = {
 //Light
 const light = new Three.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
+light.intensity = 1.25;
 scene.add(light);
 
 //Camera
@@ -66,3 +69,30 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+//Timeline
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.from(".title", { opacity: 0 }, { opacity: 1 });
+
+//Mouse-down color animation
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ];
+    let newColor = new Three.Color(`rgb(${rgb.join(",")})`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
